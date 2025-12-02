ccba77f31e27b6d3af6cceba25ce35fcd78a583e
Quentin <quentin.michon@heig-vd.ch>
2025-12-02 17:11:20 +0100

    comment EndOfGameStatus.java

--- src/main/java/ch/heigvd/puissance4engine/EndOfGameStatus.java
+++ src/main/java/ch/heigvd/puissance4engine/EndOfGameStatus.java
@@ -1,8 +1,30 @@
 package ch.heigvd.puissance4engine;
 
+/**
+ * Represents the different possible outcomes of a Puissance 4 game.
+ * <p>
+ * This enumeration is used by both the server and the clients to indicate
+ * how a game has ended.
+ */
 public enum EndOfGameStatus {
+
+    /**
+     * The player has won the game.
+     */
     WIN,
+
+    /**
+     * The player has lost the game.
+     */
     LOOSE,
+
+    /**
+     * The game ended with no winner.
+     */
     DRAW,
+
+    /**
+     * The opponent disconnected.
+     */
     FORFEIT,
 }
